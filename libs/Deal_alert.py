78a91831eb5d09f805a6d32e52fe69707e6f4a3c
cuzai <cuzai@naver.com>
2019-12-22 10:28:42 +0900

    Update Deal_alert.py - Add print_alert_all()

--- libs/Deal_alert.py
+++ libs/Deal_alert.py
@@ -1,9 +1,30 @@
 import selenium
+import time
 
 
-def deal_alert(driver):
+def print_alert(driver):
     try:
         text = driver.switch_to.alert.text
         print(text)
+    except selenium.common.exceptions.NoAlertPresentException:
+        pass
+
+
+def deal_alert_all(driver):
+    try:
+        while True:
+            driver.switch_to.alert.accept()
+            time.sleep(1)
+    except selenium.common.exceptions.NoAlertPresentException:
+        pass
+
+
+def print_alert_all(driver):
+    try:
+        while True:
+            text = driver.switch_to.alert.text
+            print(text)
+            driver.switch_to.alert.accept()
+            time.sleep(1)
     except selenium.common.exceptions.NoAlertPresentException:
         pass
