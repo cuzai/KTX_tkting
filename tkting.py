19b0738c6973837f451f53fb12246c1d4c5100a7
cuzai <cuzai@naver.com>
2019-12-22 11:43:36 +0900

    Update tkting.py_0.1.0 : Now able to purchase ticket

--- tkting.py
+++ tkting.py
@@ -1,3 +1,4 @@
+import time
 import re
 
 from Check_Chromedriver.Check_Chromedriver import Check_Chromedriver
@@ -21,14 +22,19 @@ class Tkting:
         self.add_info = {
             "id": login_info.id,
             "pwd": login_info.pwd,
-            "depart": "부산",
-            "dest": "서울",
+            "depart": "서울",
+            "dest": "부산",
             "month": "12",
-            "day": "22",
+            "day": "25",
             "depart_time_from": "18",
             "depart_time_to": "24",
             "people": "1",
-            "train": "무궁화호",
+            "train": "KTX",
+            "card_num": login_info.card_num,
+            "card_month": login_info.card_month,
+            "card_year": login_info.card_year,
+            "card_pwd": login_info.card_pwd,
+            "identity": login_info.identity,
         }
 
     def get_driver(self):
@@ -74,9 +80,10 @@ class Tkting:
                 isAvailable = normal_seat.find_element_by_css_selector(
                     "img"
                 ).get_attribute("alt")
-                if isAvailable == "예약하기d":
+                if isAvailable == "예약하기":
                     normal_seat.find_element_by_css_selector("img").click()
                     print("Got it")
+                    driver.implicitly_wait(10)
                     return True
                 print("{}. {} - {}".format(idx, train, depart_time))
                 idx += 1
@@ -84,13 +91,68 @@ class Tkting:
             if go_next:
                 Deal_btns.next_btns(driver)
 
+    def payment(self, driver):
+        # card info
+        input_card_info = driver.find_elements_by_css_selector(
+            "#Div_Card tbody.lef tr"
+        )[1].find_elements_by_css_selector("input")
+
+        card_num = self.add_info["card_num"].split("-")
+
+        # input card num
+        for n, i in enumerate(input_card_info):
+            i.send_keys(card_num[n])
+
+        # input card month
+        month_ops = driver.find_elements_by_css_selector("#month option")
+        for op in month_ops:
+            if op.text == self.add_info["card_month"]:
+                op.click()
+
+        # input card year
+        year_ops = driver.find_elements_by_css_selector("#year option")
+        for op in year_ops:
+            if op.text == self.add_info["card_year"]:
+                op.click()
+
+        # input card pwd
+        input_card_pwd = driver.find_elements_by_css_selector("#Div_Card tbody.lef tr")[
+            4
+        ].find_element_by_css_selector("input")
+        input_card_pwd.send_keys(self.add_info["card_pwd"])
+
+        # input 주민번호
+        identity_info = driver.find_elements_by_css_selector("#Div_Card tbody.lef tr")[
+            5
+        ].find_element_by_css_selector("input")
+        identity_info.send_keys(self.add_info["identity"])
+
+        # check checkbox
+        driver.find_element_by_css_selector("#chkAgree").click()
+
+        driver.find_element_by_css_selector("#fnIssuing").click()
+
+        # click the last button
+        driver.implicitly_wait(10)
+        driver.find_element_by_css_selector("#tabSale3").click()
+        driver.implicitly_wait(10)
+        time.sleep(5)
+
+        driver.switch_to.frame("mainframeSaleInfo")
+        driver.find_element_by_css_selector("#radSmart1").click()
+        driver.find_element_by_css_selector("#btn_next").click()
+        Deal_alert.print_alert_all(driver)
+
     def main(self):
         driver = self.get_driver()
         try:
             self.login(driver)
             Input_things.input_things(driver, self.add_info)
-            Deal_alert.deal_alert(driver)
-            self.reservation(driver)
+            Deal_alert.print_alert(driver)
+            if self.reservation(driver):
+                Deal_alert.print_alert_all(driver)
+                driver.find_element_by_css_selector("#btn_next").click()
+                self.payment(driver)
         finally:
             # time.sleep(60)
             driver.quit()
